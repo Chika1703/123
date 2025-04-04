68d6d0f8c8f259ea003fbc41251262b0761d89d3
Chika1703 <146464256+Chika1703@users.noreply.github.com>
2025-04-04 04:05:39 +0300

    Update outputs.tf

--- 02.1/outputs.tf
+++ 02.1/outputs.tf
@@ -1,7 +1,7 @@
 output "web_server" {
   value = {
     name        = twc_server.web_server.name
-    external_ip = twc_server.web_server.main_ipv4
+    external_ip = twc_server.web_server.networks[0].ips[0].ip
     id          = twc_server.web_server.id
   }
 }
@@ -9,7 +9,7 @@ output "web_server" {
 output "db_server" {
   value = {
     name        = twc_server.db_server.name
-    external_ip = twc_server.db_server.main_ipv4
+    external_ip = twc_server.db_server.networks[0].ips[0].ip
     id          = twc_server.db_server.id
   }
 }
