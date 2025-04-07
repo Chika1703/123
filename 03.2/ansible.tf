15536d632705286d4b789d97ca214ddaa8e4bee2
Chika1703 <146464256+Chika1703@users.noreply.github.com>
2025-04-07 21:32:56 +0300

    Update ansible.tf

--- 03.2/ansible.tf
+++ 03.2/ansible.tf
@@ -1,6 +1,7 @@
 resource "local_file" "ansible_inventory" {
   content = templatefile("${path.module}/inventory.tpl", {
     webservers_ips = twc_floating_ip.web_ips[*].ip
+    database_ip    = twc_floating_ip_db[*].ip
     storage_ip     = twc_floating_ip.storage_ip[0].ip
   })
   filename = "${path.module}/ansible_inventory"
